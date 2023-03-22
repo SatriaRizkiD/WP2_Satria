b8d478dd63bbb5a38b7d72dd0944d07fa8de9041
SatriaRizkiD <intelektual36@gmail.com>
2023-03-23 06:46:09 +0700

    Pustaka Booking

---
+++ application/models/Model_latihan1.php
@@ -0,0 +1,13 @@
+<?php
+class Model_latihan 1 extends CI_Model
+{
+    public $nilai1, $nilai2, $hasil;
+    
+    public function jumlah($n1 = null, $n2 = mill)
+    {
+        this->nilai1 = $nil1;
+        this->nilai2 = $nil2;
+        this->hasil1 = $this->nilai1 + $this->nilai2;
+        return $this->hasil;
+    }
+}
